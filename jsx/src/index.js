c9cb67e131970d5f11cd5251ccf51b38f4c2e9d3
itro-dm <dmitr.morar@gmail.com>
2018-12-26 00:18:39 +0200

    Initialize project

---
+++ jsx/src/index.js
@@ -0,0 +1,15 @@
+// Import
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create
+const App = () => {
+  return <h1>Hi there!</h1>
+};
+
+
+// Take the component and show
+ReactDOM.render(
+  <App/>,
+  document.querySelector('#root')
+);
