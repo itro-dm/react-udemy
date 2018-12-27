0db5adfeab2ef4968be5feab4daa93e7a0cb027b
itro-dm <dmitr.morar@gmail.com>
2018-12-27 06:55:29 +0200

    Uses variable, objects

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,12 +4,24 @@ import ReactDOM from 'react-dom';
 
 // Create
 const App = () => {
-  return <h1>Hi there!</h1>
+	const btnTxt = ['Hi', ' there'];
+	const btnTxt1 = {text: 'Hi there'};
+	const labelTxt = 'Enter name';
+	const style = {backgroundColor: 'blue', color: 'white'};
+
+	return (
+		<div>
+			<label className="label" htmlFor="name">{labelTxt}</label>
+			<input id="name" type="text"/>
+			<button style={{backgroundColor: 'blue', color: 'white'}}>{btnTxt}</button>
+			<button style={style}>{btnTxt1.text}</button>
+		</div>
+	);
 };
 
 
 // Take the component and show
 ReactDOM.render(
-  <App/>,
-  document.querySelector('#root')
+	<App/>,
+	document.querySelector('#root')
 );
