7700cfed34272bb29a1faa2ebb16a5d806c8f45c
itro-dm <dmitr.morar@gmail.com>
2019-02-05 09:05:21 +0200

    Section:9 Building list of record v1

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import unpslash from '../unpslash';
+import unpslash from '../api/unpslash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
 
@@ -19,7 +20,7 @@ class App extends React.Component {
 		return (
 			<div className="ui container">
 				<SearchBar onSubmit={this.onSearchSubmit} />
-				Found {this.state.images.length} images
+				<ImageList images={this.state.images} />
 			</div>
 		);
 	}
